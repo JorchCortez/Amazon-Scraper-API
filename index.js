933815974ea3cea1f85530023f5138b4f074d9ee
Joorch <jacortez_94@hotmail.com>
2021-12-04 17:58:46 -0600

    Created app with API (currently using api.scraperapi to get AMZN info) will replace with a data scraper later on
    Fully finished API with endpoints to get different information from Amazon

---
+++ index.js
@@ -0,0 +1,77 @@
+const express = require("express");
+const request = require("request-promise");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+const GenerateBaseURL = (apiKey) => `http://api.scraperapi.com?api_key=${apiKey}&autoparse=true`;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send("Welcome to Amazon Scraper API");
+})
+
+//GET Product details
+app.get('/products/:productId', async (req, res) =>{
+    const {productId} = req.params;
+    const {apiKey} = req.query;
+    try{
+        const response = await request(`${GenerateBaseURL(apiKey)}&url=https://www.amazon.com/dp/${productId}`)
+
+        res.json(JSON.parse(response))
+    }
+    catch(err){
+        res.json(err)
+    }
+})
+
+
+//GET Product reviews
+app.get('/products/:productId/reviews', async (req, res) =>{
+    const {productId} = req.params;
+    const {apiKey} = req.query;
+    
+    try{
+        const response = await request(`${GenerateBaseURL(apiKey)}&url=https://www.amazon.com/product-reviews/${productId}`)
+
+        res.json(JSON.parse(response))
+    }
+    catch(err){
+        res.json(err)
+    }
+})
+
+//GET Product offers
+app.get('/products/:productId/offers', async (req, res) =>{
+    const {productId} = req.params;
+    const {apiKey} = req.query;
+    
+    try{
+        const response = await request(`${GenerateBaseURL(apiKey)}&url=https://www.amazon.com/gp/offer-listing/${productId}`)
+
+        res.json(JSON.parse(response))
+    }
+    catch(err){
+        res.json(err)
+    }
+})
+
+
+//GET Search Results
+app.get('/search/:searchQuery', async (req, res) =>{
+    const {searchQuery} = req.params;
+    const {apiKey} = req.query;
+    
+    try{
+        const response = await request(`${GenerateBaseURL(apiKey)}&url=https://www.amazon.com/s?k=/${searchQuery}`)
+
+        res.json(JSON.parse(response))
+    }
+    catch(err){
+        res.json(err)
+    }
+})
+
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
